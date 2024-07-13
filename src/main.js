ae45cd91aab25b636388270b43fb33b6489faa2e
jan <50909186+82beja1bwi@users.noreply.github.com>
2024-07-13 21:52:23 +0200

    review functional done. cleaning.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-import run2CPerfAnalysis from "./scripts/performance_analysis/performance_2c.js";
-import run3CPerfAnalysis from "./scripts/performance_analysis/performance_3c.js";
 import run2CQualititativeAnalysis from "./scripts/qualitative_analysis/qualitative_2c.js";
 import run3CQualititativeAnalysis from "./scripts/qualitative_analysis/qualitative_3c.js";
 
@@ -7,5 +5,3 @@ import run3CQualititativeAnalysis from "./scripts/qualitative_analysis/qualitati
 
 run2CQualititativeAnalysis()
 run3CQualititativeAnalysis()
-run2CPerfAnalysis()
-run3CPerfAnalysis()
